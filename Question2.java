be51e8247b6a4e15156da8677ae973c16b1edb33
Ming Qi <133361065+lawmingqi@users.noreply.github.com>
2024-04-15 14:29:50 +0800

    Update q2

--- Question2.java
+++ Question2.java
@@ -9,8 +9,14 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
+
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter your height in meters: ");
     double height = in.nextDouble();
+    
+    System.out.print("Enter your weight in kilograms: ");  
+    double weight = in.nextDouble();
+
+    System.out.println("BMI: " + weight/(height*height));
   }
 }
