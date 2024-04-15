96cafc125cc3e40aa722690d1e5c22eca5dcb430
Ming Qi <133361065+lawmingqi@users.noreply.github.com>
2024-04-15 17:16:46 +0800

    Update q2

--- Question2.java
+++ Question2.java
@@ -12,11 +12,11 @@ public class Question2
 
     Scanner in = new Scanner(System.in);
     //System.out.print("Enter your height in meters: ");
-    double height = in.nextDouble()/100;
+    double height = in.nextDouble();
     
     //System.out.print("Enter your weight in kilograms: ");  
     double weight = in.nextDouble();
 
-    System.out.println(weight/(height*height));
+    System.out.println(weight/(height/100*height/100));
   }
 }
