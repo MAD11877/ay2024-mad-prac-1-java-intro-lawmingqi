2eb79d6ee655b9bc97fc2928715e533895ec69d1
Ming Qi <133361065+lawmingqi@users.noreply.github.com>
2024-04-16 00:21:21 +0800

    Update q4

--- Question4.java
+++ Question4.java
@@ -24,7 +24,7 @@ public class Question4
     {
       for (int j = 0; j < i; j++) 
       {
-        System.out.print("* ");
+        System.out.print("*");
       }
       System.out.println();
     }
