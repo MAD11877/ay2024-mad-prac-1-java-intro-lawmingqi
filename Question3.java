f5d4ae2f25d48800afd746e72849ddfc8b96f50d
Ming Qi <133361065+lawmingqi@users.noreply.github.com>
2024-04-16 00:11:38 +0800

    Update q3

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter an integer: ");
+    //System.out.print("Enter an integer: ");
     int num = in.nextInt();
     System.out.println(num * num);
   }
